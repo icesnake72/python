bfb0acb57eee446fafdaf2a344d4e7783ff39799
icesnake72 <icesnake72@gmail.com>
2024-01-28 13:55:33 +0900

    초기커밋

--- section04/exam01.py
+++ section04/exam01.py
@@ -45,5 +45,5 @@ eng = int(input('영어 점수를 입력하세요 >>> '))
 math = int(input('수학 점수를 입력하세요 >>> '))
 avg = (kor + eng + math) / 3.0
 result = '합격' if avg >= 80 else '불합격'
-print(f'평균은 {avg:.2f}점이고, 결과는 {result}입니다.')
+print(f'평균은 {avg:.1f}점이고, 결과는 {result}입니다.')
 
